0fd6cec245e177051331df2fe2373e8808074f12
39p <62524115+39p@users.noreply.github.com>
2021-07-08 20:20:45 -0500

    fix typos

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@ fn main() {
     unsafe {
         winapi::um::synchapi::CreateMutexA(0 as *mut winapi::um::minwinbase::SECURITY_ATTRIBUTES,1,"ROBLOX_singletonMutex".as_ptr() as *const i8);
     };
-    println!("Running, exiting this or pressing enter will terminate all games except one random. You must use different accounts for every game you launch");
+    println!("Running, exiting this or pressing enter will terminate all games except a random one. You must use different accounts for every game you launch");
     std::io::stdin().read_line(&mut String::new());
 }
